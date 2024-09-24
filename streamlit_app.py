35887b1633725d0ad9285940f1f56529e163ed7d
Jyotivk03 <jjadhav1992@outlook.com>
2024-09-24 16:46:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,70 +3,48 @@ import streamlit as st
 import requests
 from snowflake.snowpark.functions import col
  
-# Write directly to the app
 
+# Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
-     """Choose the fruits you want in your smoothie!
-     """)
+    """Choose the fruits you want in your smoothie!
+    """
+)
 
-import streamlit as st
 name_on_order = st.text_input("Name of Smoothie")
-
 st.write("The Name on your smoothie will be: ", name_on_order)
-
 cnx=st.connection("snowflake") 
 session=cnx.session()
 my_dataframe=session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-
 #st.dataframe(data=my_dataframe,use_container_width=True)
-
 #st.stop()
  
 #Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
-
 pd_df=my_dataframe.to_pandas()
-
 #st.dataframe(pd_df)
-
 #st.stop()
  
 ingredients_list=st.multiselect('Choose upto 5 ingredients: ',my_dataframe,max_selections=5)
-
 if ingredients_list:
-
     ingredients_string=''
-
     for fruit_chosen in ingredients_list:
-
         ingredients_string +=fruit_chosen + ' '
  
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
  
         st.subheader(fruit_chosen + ' Nutrition Information')
-
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
-
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
-
     #st.write(ingredients_string)
-
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
-
     #st.write(my_insert_stmt)
-
     time_to_insert=st.button('Submit Order')
-
     if time_to_insert:
-
         session.sql(my_insert_stmt).collect()
-
         st.success('Your Smoothie is ordered,'+name_on_order+'!', icon="✅")
- 
+
  
 
 
